ecd98831f4c0b27cd5184ca4e366273a3d13e9eb
Eðvald Atli Sigurvaldsson <edvaldatli@hotmail.com>
2023-10-21 14:22:39 +0000

    status completed

--- lib/functions/status.dart
+++ lib/functions/status.dart
@@ -14,11 +14,18 @@ on what hand he has, his score and the hand of the house.
 hand before showdown.)
 
  */
-List? CardNameList(List playerHand){
-
+List CardNameList(List playerHand){
+  List<String> output = [];
+  for(var card in playerHand){
+    output.add(CardNamer(card));
+  }
+  return output;
 }
 
 void Status(List<int> playerHand, List<int> houseHand){
-
+  print("Here is your hand:");
+  print(CardNameList(playerHand));
+  print("Here is the dealers hand:");
+  print(CardNameList(houseHand));
 }
 
