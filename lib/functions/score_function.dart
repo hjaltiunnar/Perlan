7c785e52056c9a42df329dbd192e1d1b5243cae3
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2023-10-21 12:18:50 +0000

    Create score_function

--- lib/functions/score_function.dart
+++ lib/functions/score_function.dart
@@ -1,4 +1,3 @@
-
 /*
 Create a function called CalculateScore that can calculate the score of a given hand of cards.
 The score is blackjack score, so the following rules apply
@@ -22,6 +21,22 @@ gets the score 1. So if the method gets the hand [1, 1] the score would be 12
   */
 
 int? CalculateScore(List<int> hand){
-
+  int sum = 0;
+  bool checkIfAce = false;
+  for (int number in hand) {
+    if (number == 1 && checkIfAce == false){
+      checkIfAce = true;
+      sum += 11;
+    } else if (number == 1 && checkIfAce == true) {
+      sum += 1;
+    } else if(number >= 2 && number <= 10) {
+      sum += number;
+    } else if (number >= 11 && number <= 13){
+      sum += 10;
+    } else if (number == 1){
+      sum += 11;
+    }
+  }
+  return sum;
 }
 
